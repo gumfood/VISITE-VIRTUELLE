43de1279cd7da20aab49e4d19846b6398df81994
gumfood <daoudifoudhil@gmail.com>
2025-12-07 15:14:30 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,47 +1,74 @@
-const rooms = {
-  "bureau-1": {
-    preview: "img/tiles/bureau-1/preview.jpg",
-    levels: {
-      1: { b: "img/tiles/bureau-1/1/b/0.jpg", d: "img/tiles/bureau-1/1/d/0.jpg", f: "img/tiles/bureau-1/1/f/0.jpg", l: "img/tiles/bureau-1/1/l/0.jpg", r: "img/tiles/bureau-1/1/r/0.jpg", u: "img/tiles/bureau-1/1/u/0.jpg" },
-      2: { b: "img/tiles/bureau-1/2/b/0.jpg", d: "img/tiles/bureau-1/2/d/0.jpg", f: "img/tiles/bureau-1/2/f/0.jpg", l: "img/tiles/bureau-1/2/l/0.jpg", r: "img/tiles/bureau-1/2/r/0.jpg", u: "img/tiles/bureau-1/2/u/0.jpg" }
+const pieces = [
+    {
+        name: "bureau-1",
+        preview: "img/bureau-1-preview.jpg",
+        levels: [
+            { level: 1, faces: ["1","3","5","l","r","u"] },
+            { level: 2, faces: ["1","3","5","l","r","u"] }
+        ]
+    },
+    {
+        name: "bureau-2-balcon",
+        preview: "img/bureau-2-preview.jpg",
+        levels: [
+            { level: 1, faces: ["b","d","f","l","r","u"] },
+            { level: 2, faces: ["b","d","f","l","r","u"] }
+        ]
+    },
+    {
+        name: "bureau-3",
+        preview: "img/bureau-3-preview.jpg",
+        levels: [
+            { level: 1, faces: ["b","d","f","l","r","u"] },
+            { level: 2, faces: ["b","d","f","l","r","u"] }
+        ]
+    },
+    {
+        name: "cuisine",
+        preview: "img/cuisine-preview.jpg",
+        levels: [
+            { level: 1, faces: ["b","d","f","l","r","u"] },
+            { level: 2, faces: ["b","d","f","l","r","u"] }
+        ]
+    },
+    {
+        name: "hall-1",
+        preview: "img/hall-1-preview.jpg",
+        levels: [
+            { level: 1, faces: ["b","d","f","l","r","u"] },
+            { level: 2, faces: ["b","d","f","l","r","u"] }
+        ]
+    },
+    {
+        name: "hall-2",
+        preview: "img/hall-2-preview.jpg",
+        levels: [
+            { level: 1, faces: ["b","d","f","l","r","u"] },
+            { level: 2, faces: ["b","d","f","l","r","u"] }
+        ]
+    },
+    {
+        name: "salle-d-attente",
+        preview: "img/salle-d-attente-preview.jpg",
+        levels: [
+            { level: 1, faces: ["b","d","f","l","r","u"] },
+            { level: 2, faces: ["b","d","f","l","r","u"] }
+        ]
+    },
+    {
+        name: "salle-reunion",
+        preview: "img/salle-reunion-preview.jpg",
+        levels: [
+            { level: 1, faces: ["b","d","f","l","r","u"] },
+            { level: 2, faces: ["b","d","f","l","r","u"] }
+        ]
+    },
+    {
+        name: "toilette-pmr",
+        preview: "img/toilette-pmr-preview.jpg",
+        levels: [
+            { level: 1, faces: ["b","d","f","l","r","u"] },
+            { level: 2, faces: ["b","d","f","l","r","u"] }
+        ]
     }
-  },
-  "bureau-2-balcon": {
-    preview: "img/tiles/bureau-2-balcon/preview.jpg",
-    levels: {
-      1: { b: "img/tiles/bureau-2-balcon/1/b/0.jpg", d: "img/tiles/bureau-2-balcon/1/d/0.jpg", f: "img/tiles/bureau-2-balcon/1/f/0.jpg", l: "img/tiles/bureau-2-balcon/1/l/0.jpg", r: "img/tiles/bureau-2-balcon/1/r/0.jpg", u: "img/tiles/bureau-2-balcon/1/u/0.jpg" },
-      2: { b: "img/tiles/bureau-2-balcon/2/b/0.jpg", d: "img/tiles/bureau-2-balcon/2/d/0.jpg", f: "img/tiles/bureau-2-balcon/2/f/0.jpg", l: "img/tiles/bureau-2-balcon/2/l/0.jpg", r: "img/tiles/bureau-2-balcon/2/r/0.jpg", u: "img/tiles/bureau-2-balcon/2/u/0.jpg" }
-    }
-  },
-  "bureau-3": {
-    preview: "img/tiles/bureau-3/preview.jpg",
-    levels: {
-      1: { b: "img/tiles/bureau-3/1/b/0.jpg", d: "img/tiles/bureau-3/1/d/0.jpg", f: "img/tiles/bureau-3/1/f/0.jpg", l: "img/tiles/bureau-3/1/l/0.jpg", r: "img/tiles/bureau-3/1/r/0.jpg", u: "img/tiles/bureau-3/1/u/0.jpg" },
-      2: { b: "img/tiles/bureau-3/2/b/0.jpg", d: "img/tiles/bureau-3/2/d/0.jpg", f: "img/tiles/bureau-3/2/f/0.jpg", l: "img/tiles/bureau-3/2/l/0.jpg", r: "img/tiles/bureau-3/2/r/0.jpg", u: "img/tiles/bureau-3/2/u/0.jpg" }
-    }
-  },
-  "cuisine": {
-    preview: "img/tiles/cuisine/preview.jpg",
-    levels: {
-      1: { b: "img/tiles/cuisine/1/b/0.jpg", d: "img/tiles/cuisine/1/d/0.jpg", f: "img/tiles/cuisine/1/f/0.jpg", l: "img/tiles/cuisine/1/l/0.jpg", r: "img/tiles/cuisine/1/r/0.jpg", u: "img/tiles/cuisine/1/u/0.jpg" },
-      2: { b: "img/tiles/cuisine/2/b/0.jpg", d: "img/tiles/cuisine/2/d/0.jpg", f: "img/tiles/cuisine/2/f/0.jpg", l: "img/tiles/cuisine/2/l/0.jpg", r: "img/tiles/cuisine/2/r/0.jpg", u: "img/tiles/cuisine/2/u/0.jpg" }
-    }
-  },
-  "hall-1": {
-    preview: "img/tiles/hall-1/preview.jpg",
-    levels: {
-      1: { b: "img/tiles/hall-1/1/b/0.jpg", d: "img/tiles/hall-1/1/d/0.jpg", f: "img/tiles/hall-1/1/f/0.jpg", l: "img/tiles/hall-1/1/l/0.jpg", r: "img/tiles/hall-1/1/r/0.jpg", u: "img/tiles/hall-1/1/u/0.jpg" },
-      2: { b: "img/tiles/hall-1/2/b/0.jpg", d: "img/tiles/hall-1/2/d/0.jpg", f: "img/tiles/hall-1/2/f/0.jpg", l: "img/tiles/hall-1/2/l/0.jpg", r: "img/tiles/hall-1/2/r/0.jpg", u: "img/tiles/hall-1/2/u/0.jpg" }
-    }
-  },
-  "hall-2": {
-    preview: "img/tiles/hall-2/preview.jpg",
-    levels: {
-      1: { b: "img/tiles/hall-2/1/b/0.jpg", d: "img/tiles/hall-2/1/d/0.jpg", f: "img/tiles/hall-2/1/f/0.jpg", l: "img/tiles/hall-2/1/l/0.jpg", r: "img/tiles/hall-2/1/r/0.jpg", u: "img/tiles/hall-2/1/u/0.jpg" },
-      2: { b: "img/tiles/hall-2/2/b/0.jpg", d: "img/tiles/hall-2/2/d/0.jpg", f: "img/tiles/hall-2/2/f/0.jpg", l: "img/tiles/hall-2/2/l/0.jpg", r: "img/tiles/hall-2/2/r/0.jpg", u: "img/tiles/hall-2/2/u/0.jpg" }
-    }
-  },
-  "salle-d-attente": {
-    preview: "img/tiles/salle-d-attente/preview.jpg",
-    levels: {
-      1: { b: "img/tiles/salle-d-attente/1/b/0.jpg", d: "img/tiles/salle-d-attente/1/d/0.jpg", f: "img/tiles/salle-d-attente/1
+];
